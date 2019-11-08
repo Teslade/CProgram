46667c3471a516e0d87b8972b9aec690277fcba3
unknown <ldt@cndatacom.com>
2019-11-08 19:02:48 +0800

    Initial commit

---
+++ main.c
@@ -0,0 +1,21 @@
+#include <stdio.h>    /* printf, scanf definitions */
+#define KMS_PER_MILE 1.609 /* conversion constant  */
+
+int
+main(void)
+{
+    double miles,  /* distance in miles	*/
+            kms;    /* equivalent distance in kilometers	*/
+
+    /* Get and echo the distance in miles. */
+    scanf("%lf", &miles);
+    printf("The distance in miles is %.2f.\n", miles);
+
+    /* Convert the distance to kilometers. */
+    kms = KMS_PER_MILE * miles;
+
+    /* Display the distance in kilometers. */
+    printf("That equals %.2f kilometers.\n", kms);
+
+    return (0);
+}
