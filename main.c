3d5bce2ad25377a13a0468fd09aeb405a6865f35
teslade <teslade@163.com>
2019-11-08 19:39:06 +0800

    2-12

--- main.c
+++ main.c
@@ -1,21 +1,55 @@
-#include <stdio.h>    /* printf, scanf definitions */
-#define KMS_PER_MILE 1.609 /* conversion constant  */
+Figure 2.12
+/*
+ * Determines the value of a collection of coins.
+ */
+#include <stdio.h>
 
 int
 main(void)
 {
-    double miles,  /* distance in miles	*/
-            kms;    /* equivalent distance in kilometers	*/
+    char first, middle, last;	/* input - 3 initials     	*/
+    int pennies, nickels;  /* input - count of each coin type	*/
+    int dimes, quarters;   /* input - count of each coin type	*/
+    int change; 	/* output - change amount    	*/
+    int dollars;        	/* output - dollar amount   	*/
+    int total_cents;  	/* total cents             	*/
 
-    /* Get and echo the distance in miles. */
-    scanf("%lf", &miles);
-    printf("The distance in miles is %.2f.\n", miles);
+    /* Get and display the customer's initials. */
+    printf("Type in 3 initials and press return> ");
+    scanf("%c%c%c", &first, &middle, &last);
+    printf("Hello %c%c%c, let's see what your coins are worth.\n",
+           first, middle, last);
 
-    /* Convert the distance to kilometers. */
-    kms = KMS_PER_MILE * miles;
+    /* Get the count of each kind of coin. */
+    printf("Number of quarters> ");
+    scanf("%d", &quarters);
+    printf("Number of dimes   > ");
+    scanf("%d", &dimes);
+    printf("Number of nickels > ");
+    scanf("%d", &nickels);
+    printf("Number of pennies > ");
+    scanf("%d", &pennies);
 
-    /* Display the distance in kilometers. */
-    printf("That equals %.2f kilometers.\n", kms);
+    /* Compute the total value in cents. */
+    total_cents = 25 * quarters + 10 * dimes +
+                  5 * nickels + pennies;
+
+    /* Find the value in dollars and change. */
+    dollars = total_cents / 100;
+    change = total_cents % 100;
+
+    /* Display the value in dollars and change. */
+    printf("\nYour coins are worth %d dollars and %d cents.\n",
+           dollars, change);
 
     return (0);
 }
+
+Type in 3 initials and press Return> BMC
+        Hello BMC, let's see what your coins are worth.
+Number of quarters> 8
+Number of dimes   > 20
+Number of nickels > 30
+Number of pennies > 77
+
+Your coins are worth 6 dollars and 27 cents.
