91b6df090c5a57212aa9f5662ceabda0855c5214
teslade <teslade@163.com>
2019-11-08 19:51:22 +0800

    2-15

--- main.c
+++ main.c
@@ -1,21 +1,32 @@
-#include <stdio.h>    /* printf, scanf definitions */
-#define KMS_PER_MILE 1.609 /* conversion constant  */
+Figure 2.15  Compiler Listing of a Program with Syntax Errors
+221 /* Converts distances from miles to kilometers. */
+222
+223 #include <stdio.h>         /* printf, scanf definitions   */
+266 #define KMS_PER_MILE 1.609 /* conversion constant         */
+267
+268 int
+269 main(void)
+270 {
+271       double kms
+272
+273       /* Get the distance in miles. */
+274       printf("Enter the distance in miles> ");
+***** Semicolon added at the end of the previous source line
 
-int
-main(void)
-{
-    double miles,  /* distance in miles	*/
-            kms;    /* equivalent distance in kilometers	*/
+275       scanf("%lf", &miles);
+***** Identifier "miles" is not declared within this scope
+***** Invalid operand of address-of operator
 
-    /* Get and echo the distance in miles. */
-    scanf("%lf", &miles);
-    printf("The distance in miles is %.2f.\n", miles);
+276
+277       /* Convert the distance to kilometers. */
+278       kms = KMS_PER_MILE * miles;
+***** Identifier "miles" is not declared within this scope
 
-    /* Convert the distance to kilometers. */
-    kms = KMS_PER_MILE * miles;
-
-    /* Display the distance in kilometers. */
-    printf("That equals %.2f kilometers.\n", kms);
-
-    return (0);
-}
+279
+280       /* Display the distance in kilometers. * /
+281       printf("That equals %f kilometers.\n", kms);
+282
+283       return (0);
+284 }
+***** Unexpected end-of-file encountered in a comment
+***** "}" inserted before end-of-file
